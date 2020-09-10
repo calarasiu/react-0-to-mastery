5c5b362f6016806bd2c144d44e1a1c82b463fe74
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-10 16:18:55 +0100

    failed to add tachyons

--- robofriends/src/Hello.js
+++ robofriends/src/Hello.js
@@ -4,7 +4,7 @@ import './Hello.css';
 class Hello extends Component {
   render(){
     return(
-      <div className= 'f1 tc'>
+      <div>
         <h1>Hello World</h1>
         <p>welcome to React</p>
       </div>
