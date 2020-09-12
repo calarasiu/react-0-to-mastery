a3e5207970f8e39fe8948c88e132ca52d8031374
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-12 11:35:06 +0100

    add robo images and identities

---
+++ robofriends/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({name, email, id}) =>{
+  // we can deconstruct the props directly in the parameter or by storing them in a variable -> const {name, email, id} = props;
+  return (
+    <div className = 'tc bg-light-green dib br3 pa-3 grow bw-2 shadow-5 mh3 mv2'>
+      <img src={`https://robohash.org/${id}`} alt="robots"/>
+      <div>
+  <h2>{name}</h2>
+  <p>{email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ robofriends/src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {id : 1,
+    name : 'Bright Bulb',
+    username : 'Bulby',
+    email : 'bright.bulb@gmail.com'
+  },
+  {id : 2,
+    name : 'Yellowish Iron',
+    username : 'Yell',
+    email : 'yellowish.iron@gmail.com'
+},
+{id : 3,
+  name : 'Booky Glassowich',
+  username : 'Booky',
+  email : 'booky.glassowich@gmail.com'
+},
+{id : 4,
+  name : 'Smarty Pants',
+  username : 'Smarty',
+  email : 'smarty.pants@gmail.com'
+},
+{id : 5,
+  name : 'Smiley Face',
+  username : 'Smiley',
+  email : 'smiley.face@gmail.com'
+},
+{id : 6,
+  name : 'Chatty Box',
+  username : 'Chatty',
+  email : 'chatty.box@gmail.com'
+}
+]
