97bb5fd988ac899693729e5c9536316755f956e2
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-22 22:17:40 +0100

    build the app and organize the files in components and containers

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import {robots} from './robots.js';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import {robots} from '../robots.js';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component{
   constructor(){
@@ -25,8 +25,9 @@ class App extends Component{
   }
   
   render(){
-    const filteredRobots = this.state.robots.filter(robots =>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+   const {robots, searchfield} = this.state;  //destructuring
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return(
       <div className='tc'>
